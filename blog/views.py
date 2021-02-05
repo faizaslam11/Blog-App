0d4aa7019929df7f36eeb453cbce97ef0a63b52f
faizaslam11 <faizaslam11@gmail.com>
2021-02-06 01:17:49 +0530

    a

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views.generic import (ListView, 
+                                DetailView, 
+                                CreateView
+)
 from .models import Post
 
 
@@ -34,11 +37,12 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model= Post 
-    
 
-class PostCreateView(DetailView):
-    model = Post
+
+class PostCreateView(CreateView):
+    model= Post     
     fields = ['title', 'content']
+    
     
 
 def about(request):
