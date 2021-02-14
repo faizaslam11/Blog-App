142c4d9cb1b884eae352956615faec697c5cc0e4
faizaslam11 <faizaslam11@gmail.com>
2021-02-14 21:09:15 +0530

    p

--- django_project/urls.py
+++ django_project/urls.py
@@ -30,10 +30,14 @@ urlpatterns = [
         auth_views.PasswordResetView.as_view(
             template_name='users/password_reset.html'), 
          name='password-reset'),
-    path('password-reset/done/', 
+    path('password_reset/done/', 
         auth_views.PasswordResetDoneView.as_view(
             template_name='users/password_reset_done.html'), 
-         name='password-reset_done'),     
+         name='password_reset_done'),    
+    path('password-reset-confirm/<uidb64>/<token>/', 
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html'), 
+         name='password_reset_confirm'),        
 
     path('', include('blog.urls')),
 ]
